e7e94908ae391bd0fbc485921008200866b4e44f
Andrii Zaika <lytiy7404@gmail.com>
2025-06-29 21:30:07 +0300

    init spring boot app

---
+++ src/main/java/ua/azaika/serverpulse/ServerPulseApplication.java
@@ -0,0 +1,13 @@
+package ua.azaika.serverpulse;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServerPulseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServerPulseApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ua/azaika/serverpulse/ServerPulseApplicationTests.java
@@ -0,0 +1,13 @@
+package ua.azaika.serverpulse;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServerPulseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
