b5109d661de2a29345ef6c6af861135d1bff3f83
Andrii Zaika <lytiy7404@gmail.com>
2025-07-15 13:01:41 +0300

    Extend `AuthenticationServiceTest` with additional unit tests covering sign-up and sign-in scenarios

--- src/test/java/ua/azaika/serverpulse/service/AuthenticationServiceTest.java
+++ src/test/java/ua/azaika/serverpulse/service/AuthenticationServiceTest.java
@@ -8,6 +8,10 @@ import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import ua.azaika.serverpulse.dto.auth.JwtAuthenticationResponseDTO;
 import ua.azaika.serverpulse.dto.auth.SignInRequestDTO;
@@ -15,6 +19,7 @@ import ua.azaika.serverpulse.dto.auth.SignUpRequestDTO;
 import ua.azaika.serverpulse.dto.auth.UserResponseDTO;
 import ua.azaika.serverpulse.entity.Role;
 import ua.azaika.serverpulse.entity.UserEntity;
+import ua.azaika.serverpulse.exception.UserAlreadyExistsException;
 import ua.azaika.serverpulse.security.CustomUserDetails;
 
 import java.time.LocalDateTime;
@@ -22,6 +27,7 @@ import java.util.List;
 import java.util.UUID;
 
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.anyString;
 import static org.mockito.Mockito.*;
@@ -90,9 +96,47 @@ class AuthenticationServiceTest {
             assertThat(response.token()).isEqualTo("jwtToken");
 
         }
+
+        @Test
+        void should_throw_if_user_already_exists() {
+            when(passwordEncoder.encode(anyString())).thenReturn("encodedPassword");
+            doThrow(new UserAlreadyExistsException("User already exists")).when(userService).create(any(UserEntity.class));
+
+            assertThrows(UserAlreadyExistsException.class, () -> authenticationService.signUp(signUpRequestDTO));
+
+            verify(passwordEncoder, times(1)).encode(signInRequestDTO.password());
+            verify(userService, times(1)).create(any(UserEntity.class));
+            verify(jwtService, never()).generateToken(any(CustomUserDetails.class));
+        }
     }
 
-    @Test
-    void signIn() {
+    @Nested
+    class SignInTest {
+        @Test
+        void should_authenticate_user_and_generate_token_when_credentials_are_valid() {
+            Authentication authentication = mock(Authentication.class);
+            when(authentication.getPrincipal()).thenReturn(userDetails);
+            when(authenticationManager.authenticate(any(UsernamePasswordAuthenticationToken.class))).thenReturn(authentication);
+
+            when(jwtService.generateToken(any(CustomUserDetails.class))).thenReturn("mockedJwtToken");
+
+            JwtAuthenticationResponseDTO response = authenticationService.signIn(signInRequestDTO);
+
+            verify(authenticationManager, times(1)).authenticate(any(UsernamePasswordAuthenticationToken.class));
+            verify(jwtService, times(1)).generateToken(any(CustomUserDetails.class));
+
+            assertThat(response).isNotNull();
+            assertThat(response.token()).isEqualTo("mockedJwtToken");
+        }
+
+        @Test
+        void should_throw_BadCredentialsException_when_credentials_are_invalid() {
+            when(authenticationManager.authenticate(any(UsernamePasswordAuthenticationToken.class)))
+                    .thenThrow(new BadCredentialsException("Invalid credentials"));
+
+            assertThrows(BadCredentialsException.class, () -> authenticationService.signIn(signInRequestDTO));
+
+            verify(jwtService, never()).generateToken(any(UserDetails.class));
+        }
     }
 }
